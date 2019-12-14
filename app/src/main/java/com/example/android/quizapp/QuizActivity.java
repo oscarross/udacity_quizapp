b8be24266f2d2f9f04f9e3ba03501d7bf884a7c9
Oskar Rosolowski <oskar.rosolowski@stepstone.com>
2019-12-14 01:48:19 +0100

    Fragment

--- app/src/main/java/com/example/android/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/android/quizapp/QuizActivity.java
@@ -3,11 +3,18 @@ package com.example.android.quizapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.android.quizapp.Data.DatabaseStore;
 
-public class QuizActivity extends Activity {
+public class QuizActivity extends FragmentActivity
+        implements QuestionMultiCheck.OnFragmentInteractionListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,14 +22,30 @@ public class QuizActivity extends Activity {
         setContentView(R.layout.activity_quiz);
 
         // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.USERNAME);
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MainActivity.USERNAME);
 
 //        Question[] list = QuestionsFactory.make();
 //        message = list[0].getQuestion();
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.username_textview);
-        textView.setText(message);
+//        TextView textView = findViewById(R.id.username_textview);
+//        textView.setText(message);
+
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (findViewById(R.id.fragment_container)!= null) {
+            QuestionMultiCheck firstFragment = new QuestionMultiCheck();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, firstFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void didSubmit(boolean isCorrect) {
+        Log.d("QuizActivity", "didSubmit");
     }
 }
